f35f248b476bc9ba1978f73231d1def3b3cb1ab2
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-03-16 12:05:13 +0100

    fix: proxy path

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const environment =  process.env.ENVIRONMENT  || 'production';
 export const clientKey =  'proxy-as2das2d-43';
-export const unleashUrl =  'https://us.app.unleash-hosted.com/ushosted/api/proxy';
+export const unleashUrl =  'https://us.app.unleash-hosted.com/ushosted/proxy';
