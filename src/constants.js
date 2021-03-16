640214608ed1b46bdba38232d07b726f6866ac54
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-03-16 12:47:59 +0100

    make dev

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const environment =  process.env.ENVIRONMENT  || 'production';
+export const environment =  'development';
 export const clientKey =  'proxy-as2das2d-43';
 export const unleashUrl =  'https://us.app.unleash-hosted.com/ushosted/proxy';
